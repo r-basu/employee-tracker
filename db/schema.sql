fe8913ae2cf5fd8a00983fa97b59ad914066e3c7
Rahul Basu <142247733+r-basu@users.noreply.github.com>
2024-01-26 16:03:07 -0500

    Start schema work

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,9 @@ DROP DATABASE IF EXISTS employee_tracker_db;
 CREATE DATABASE employee_tracker_db;
 
 USE employee_tracker_db;
+
+CREATE TABLE department ();
+
+CREATE TABLE role ();
+
+CREATE TABLE employee ();
