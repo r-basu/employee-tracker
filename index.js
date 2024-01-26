0393c88a38ff5d7898e2b9876743cbff8dba713c
Rahul Basu <142247733+r-basu@users.noreply.github.com>
2024-01-26 17:24:00 -0500

    Add starting inquirer prompts and view all employees/quit

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const express = require("express");
 
@@ -17,6 +16,61 @@ const db = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
+
+async function runApp() {
+  try {
+    console.log("Employee Tracker Management System");
+    await runPrompt();
+  } catch (error) {
+    console.error("Error occured:", error);
+    throw error;
+  }
+}
+
+async function runPrompt() {
+  try {
+    const { default: inquirer } = await import("inquirer");
+    const startInputs = await inquirer.prompt([
+      {
+        type: "list",
+        name: "start",
+        message: "What would you like to do?",
+        choices: [
+          "View All Employees",
+          "Add Employee",
+          "Update Employee Role",
+          "View All Roles",
+          "Add Role",
+          "View All Departments",
+          "Add Department",
+          "Quit",
+        ],
+      },
+    ]);
+
+    if (startInputs.start === "View All Employees") {
+      db.query(`SELECT * FROM employee`, (error, employees) => {
+        if (error) {
+          console.log(error);
+          throw error;
+        } else {
+          console.clear(); // Maybe remove this later - see how it functions after additional prompts
+          console.table(employees);
+        }
+        runPrompt();
+      });
+    }
+
+    if (startInputs.start === "Quit") {
+      process.exit(0);
+    }
+  } catch (error) {
+    console.error("Error occured:", error);
+    throw error;
+  }
+}
+
+runApp();
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
