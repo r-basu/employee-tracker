c87567852ee140b9ac15b973da71d02204eef43c
Rahul Basu <142247733+r-basu@users.noreply.github.com>
2024-01-26 16:28:40 -0500

    add express and express setup

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 require("dotenv").config();
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
+const express = require('express')
 
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
 const db = mysql.createConnection({
     host: 'localhost',
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 })
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
