16458c274f2b53d7a205cfe04765ace3bd3cd5a7
Rahul Basu <142247733+r-basu@users.noreply.github.com>
2024-01-26 16:31:52 -0500

    setup seeds file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
-const express = require('express')
+const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -12,17 +12,17 @@ app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-})
+  host: "localhost",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
-    res.status(404).end();
-  });
-  
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
