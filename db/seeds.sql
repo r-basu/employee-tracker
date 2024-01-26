fc9ed63889140302d709e6004ba8b3f5e1e14bdb
Rahul Basu <142247733+r-basu@users.noreply.github.com>
2024-01-26 16:53:14 -0500

    finished seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,24 @@
 INSERT INTO department(department_name)
-VALUES
+VALUES  ("Accounting"),
+        ("Sales and Purchasing"),
+        ("Research and Development"),
+        ("Shipping and Receiving");
 
 INSERT INTO employee_role(title, salary, department_id)
-VALUES
+VALUES  ("Accountant", 75000, 1),
+        ("Sales Manager", 70000, 2),
+        ("Sales Assistant", 50000, 2),
+        ("Lab Manager", 80000, 3),
+        ("Lab Assistant", 50000, 3),
+        ("Shipper", 60000, 4),
+        ("Receiver", 60000, 4);
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES
+VALUES  ("Marciana", "Tatius", 1, NULL),
+        ("Aquilina", "Petronius", 2, NULL),
+        ("Lucilius", "Julius", 3, 2),
+        ("Claudia", "Domitilla", 4, NULL),
+        ("Severianus", "Aquilinus", 5, 4),
+        ("Lucilius", "Quinctilianus", 6, NULL),
+        ("Gratianus", "Terentius", 7, NULL);
+
