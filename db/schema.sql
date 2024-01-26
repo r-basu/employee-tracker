7fbd43667f657398cc6ec8a7826754b4feceefa1
Rahul Basu <142247733+r-basu@users.noreply.github.com>
2024-01-26 16:19:44 -0500

    finish schema layout

--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,32 @@ CREATE DATABASE employee_tracker_db;
 
 USE employee_tracker_db;
 
-CREATE TABLE department ();
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL
+);
 
-CREATE TABLE role ();
+-- name and role seem to be keywords so changed to department_name and employee_role
 
-CREATE TABLE employee ();
+CREATE TABLE employee_role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2),
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+-- ON DELETE SET NULL instead of CASCADE because department id's and role id's would still exist if an employee or role is removed respectively
+
+CREATE TABLE employee (
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        first_name VARCHAR(30) NOT NULL,
+        last_name VARCHAR(30) NOT NULL,
+        role_id INT NOT NULL,
+        manager_id INT,
+        FOREIGN KEY (role_id) REFERENCES employee_role(id)
+        ON DELETE SET NULL,
+        FOREIGN KEY (manager_id) REFERENCES employee(id)
+        ON DELETE SET NULL
+);
