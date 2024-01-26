8e37367c954133f0b99f05a3d42252fab9f775bb
Rahul Basu <142247733+r-basu@users.noreply.github.com>
2024-01-26 16:20:42 -0500

    remove not null on role_id if setting null on delete

--- db/schema.sql
+++ db/schema.sql
@@ -25,7 +25,7 @@ CREATE TABLE employee (
         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
         first_name VARCHAR(30) NOT NULL,
         last_name VARCHAR(30) NOT NULL,
-        role_id INT NOT NULL,
+        role_id INT,
         manager_id INT,
         FOREIGN KEY (role_id) REFERENCES employee_role(id)
         ON DELETE SET NULL,
