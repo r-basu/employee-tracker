ba9ad2ae8bfdf2e37f94ee32113d369e33edc30c
Rahul Basu <142247733+r-basu@users.noreply.github.com>
2024-02-01 17:11:58 -0500

    Add remaining functions/prompts for Add Emp/Dept/Role and Update Emp

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 require("dotenv").config();
 const mysql = require("mysql2");
-const express = require("express");
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection({
@@ -48,19 +40,308 @@ async function runPrompt() {
       },
     ]);
 
+    //VIEW ALL
     if (startInputs.start === "View All Employees") {
       db.query(`SELECT * FROM employee`, (error, employees) => {
         if (error) {
           console.log(error);
           throw error;
         } else {
-          console.clear(); // Maybe remove this later - see how it functions after additional prompts
+          console.clear(); // Clears console of previous tables to only show current prompts results - Nice!
           console.table(employees);
         }
         runPrompt();
       });
     }
 
+    if (startInputs.start === "View All Roles") {
+      db.query(`SELECT * FROM employee_role`, (error, employees) => {
+        if (error) {
+          console.log(error);
+          throw error;
+        } else {
+          console.clear();
+          console.table(employees);
+        }
+        runPrompt();
+      });
+    }
+
+    if (startInputs.start === "View All Departments") {
+      db.query(`SELECT * FROM department`, (error, employees) => {
+        if (error) {
+          console.log(error);
+          throw error;
+        } else {
+          console.clear();
+          console.table(employees);
+        }
+        runPrompt();
+      });
+    }
+
+    // ADD
+    // ADD EMPLOYEE
+    const getRoles = async () => {
+      try {
+        return new Promise((resolve, reject) => {
+          db.query(`SELECT id, title FROM employee_role`, (error, data) => {
+            if (error) {
+              reject(error);
+              console.log(error);
+              throw error;
+            }
+            resolve(data);
+          });
+        });
+      } catch (error) {
+        console.log(error);
+        throw error;
+      }
+    };
+
+    const getManagers = async () => {
+      try {
+        return new Promise((resolve, reject) => {
+          db.query(
+            `SELECT id, CONCAT(first_name, ' ', last_name) AS manager_name FROM employee WHERE manager_id IS NULL`,
+            (error, data) => {
+              if (error) {
+                reject(error);
+                console.log(error);
+                throw error;
+              }
+
+              const managerNames = data.map((manager) => manager.manager_name);
+              managerNames.unshift("None"); // Add blank option
+              resolve(managerNames);
+            }
+          );
+        });
+      } catch (error) {
+        console.log(error);
+        throw error;
+      }
+    };
+
+    if (startInputs.start === "Add Employee") {
+      const roles = await getRoles();
+      const managers = await getManagers();
+      const addEmpInput = await inquirer.prompt([
+        {
+          type: "input",
+          name: "addEmpFirstName",
+          message: "Enter employee's first name:",
+        },
+        {
+          type: "input",
+          name: "addEmpLastName",
+          message: "Enter employee's last name:",
+        },
+        {
+          type: "list",
+          name: "addEmpRole",
+          message: "Select employee's role",
+          choices: roles.map((role) => role.title),
+        },
+        {
+          type: "list",
+          name: "addEmpManager",
+          message: "Select employee's manager",
+          choices: managers,
+        },
+      ]);
+
+      const selectedRole = roles.find(
+        (role) => role.title === addEmpInput.addEmpRole
+      );
+      const roleId = selectedRole.id;
+
+      let managerId = null;
+      if (addEmpInput.addEmpManager !== "None") {
+        const selectedManager = await db
+          .promise()
+          .query(
+            `SELECT id FROM employee WHERE CONCAT(first_name, ' ', last_name) = ?`,
+            [addEmpInput.addEmpManager]
+          );
+        managerId = selectedManager[0][0].id;
+      }
+      db.query(
+        `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`,
+        [
+          addEmpInput.addEmpFirstName,
+          addEmpInput.addEmpLastName,
+          roleId,
+          managerId,
+        ],
+        (error) => {
+          if (error) {
+            console.log(error);
+            throw error;
+          }
+          console.log(
+            `${addEmpInput.addEmpFirstName} ${addEmpInput.addEmpLastName} added.`
+          );
+          runPrompt();
+        }
+      );
+    }
+
+    // ADD DEPARTMENT
+    if (startInputs.start === "Add Department") {
+      const addDeptInput = await inquirer.prompt([
+        {
+          type: "input",
+          name: "addDeptName",
+          message: "Enter department name:",
+        },
+      ]);
+
+      db.query(
+        `INSERT INTO department (department_name) VALUES (?)`,
+        [addDeptInput.addDeptName],
+        (error) => {
+          if (error) {
+            console.log(error);
+            throw error;
+          }
+          console.log(`Department "${addDeptInput.addDeptName}" added.`);
+          runPrompt();
+        }
+      );
+    }
+
+    // ADD ROLE
+    const getDepartments = async () => {
+      try {
+        return new Promise((resolve, reject) => {
+          db.query(
+            `SELECT id, department_name AS name FROM department`,
+            (error, data) => {
+              if (error) {
+                reject(error);
+                console.log(error);
+                throw error;
+              }
+              resolve(data);
+            }
+          );
+        });
+      } catch (error) {
+        console.log(error);
+        throw error;
+      }
+    };
+    if (startInputs.start === "Add Role") {
+      const departments = await getDepartments();
+      const addRoleInput = await inquirer.prompt([
+        {
+          type: "input",
+          name: "addRoleTitle",
+          message: "Enter role title:",
+        },
+        {
+          type: "input",
+          name: "addRoleSalary",
+          message: "Enter role salary:",
+          validate: (value) => {
+            const parsedSalary = parseFloat(value);
+            if (isNaN(parsedSalary) || parsedSalary <= 0) {
+              return "Please enter a valid postive decimal number for salary.";
+            }
+            return true;
+          },
+        },
+        {
+          type: "list",
+          name: "addRoleDepartment",
+          message: "Select department for role",
+          choices: departments.map((dept) => dept.name),
+        },
+      ]);
+
+      const selectedDepartment = departments.find(
+        (dept) => dept.name === addRoleInput.addRoleDepartment
+      );
+      const departmentId = selectedDepartment.id;
+
+      db.query(
+        `INSERT INTO employee_role (title, salary, department_id) VALUES (?, ?, ?)`,
+        [addRoleInput.addRoleTitle, addRoleInput.addRoleSalary, departmentId],
+        (error) => {
+          if (error) {
+            console.log(error);
+            throw error;
+          }
+          console.log(`Role "${addRoleInput.addRoleTitle}" added.`);
+          runPrompt();
+        }
+      );
+    }
+
+    //UPDATE EMPLOYEE
+    const getEmployees = async () => {
+      try {
+        return new Promise((resolve,reject) => {
+          db.query(
+            `SELECT id, first_name, last_name FROM employee`,
+            (error, data) => {
+              if (error) {
+                reject (error)
+                console.log(error)
+                throw error;
+              }
+              resolve(data)
+            }
+          )
+        })
+      } catch (error) {
+        console.log(error)
+        throw error
+      }
+    }
+    if (startInputs.start === "Update Employee Role") {
+      const employees = await getEmployees();
+      const roles = await getRoles();
+      const updateEmpInput = await inquirer.prompt([
+        {
+          type: "list",
+          name: "updateEmpName",
+          message: "Select employee to update:",
+          choices: employees.map((emp) => `${emp.first_name} ${emp.last_name}`),
+        },
+        {
+          type: "list",
+          name: "updateEmpRole",
+          message: "Select employee's new role:",
+          choices: roles.map((role) => role.title),
+        },
+      ]);
+    
+      const selectedEmployee = employees.find(
+        (emp) => `${emp.first_name} ${emp.last_name}` === updateEmpInput.updateEmpName
+      );
+      const employeeId = selectedEmployee.id;
+    
+      const selectedRole = roles.find((role) => role.title === updateEmpInput.updateEmpRole);
+      const roleId = selectedRole.id;
+    
+      db.query(
+        `UPDATE employee SET role_id = ? WHERE id = ?`,
+        [roleId, employeeId],
+        (error) => {
+          if (error) {
+            console.log(error);
+            throw error;
+          }
+          console.log(`Employee "${updateEmpInput.updateEmpName}" updated.`);
+          runPrompt();
+        }
+      );
+    }
+
+    //QUIT
     if (startInputs.start === "Quit") {
       process.exit(0);
     }
@@ -71,12 +352,3 @@ async function runPrompt() {
 }
 
 runApp();
-
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-  res.status(404).end();
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
